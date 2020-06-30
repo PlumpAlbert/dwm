69baf7ee9dc0ab5f61093d799c380061c3604418
plump_albert <plumpalbert@gmail.com>
2020-06-30 16:02:53 +0300

    Updated config

--- config.h
+++ config.h
@@ -13,17 +13,16 @@ static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = {
-  "Cartograph Mono CF:size=9",
-  "monospace:size=9",
-  "Twitter Color Emoji:pixelsize=10:antialias=true:autohint=true",
-  "EmojiOne:pixelsize=10:antialias=true:autohint=true",
-  "FontAwesome:pixelsize=10:antialias=true:autohint=true",
-  "Font Awesome 5 Free:pixelsize=10:antialias=true:autohint=true",
-  "Font Awesome 5 Brands:pixelsize=10:antialias=true:autohint=true",
+  "JetBrains Mono:size=8",
+  "JoyPixels:size=8:antialias=true:autohint=true",
+  "EmojiOne:size=8:antialias=true:autohint=true",
+  "FontAwesome:size=8:antialias=true:autohint=true",
+  "Font Awesome 5 Free:size=8:antialias=true:autohint=true",
+  "Font Awesome 5 Brands:size=8:antialias=true:autohint=true",
 };
-static char dmenufont[]             = "Cartograph Mono CF:size=9";
+static char dmenufont[]             = "JetBrains Mono:size=8";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
@@ -58,12 +57,12 @@ static const Rule rules[] = {
 	*/
 	/* class    instance      title       	 tags mask    isfloating   isterminal  noswallow xkb_layout monitor */
 	{ "Gimp",     NULL,       NULL,       	    1 << 8,       0,           0,         0,         0        -1 },
-	{ "RescueTime",NULL,      NULL,       	    0,            1,           0,         0,         0        -1 },
+	{ "RescueTime",NULL,      NULL,       	    0,            1,           0,         1,         0        -1 },
 	{ "St",       NULL,       NULL,       	    0,            0,           1,         0,         0        -1 },
-	{ "firefox",  NULL,       NULL,       	    0,            0,           1,         0,        -1        -1 },
-	{ "Google-chrome",  NULL, NULL,       	    0,            0,           1,         0,        -1        -1 },
-	{ "Telegram", NULL,       NULL,       	    0,            0,           1,         0,         1        -1 },
-	{ "Discord",  NULL,       NULL,       	    0,            0,           1,         0,        -1        -1 },
+	{ "firefox",  NULL,       NULL,       	    0,            0,           1,         1,        -1        -1 },
+	{ "Google-chrome",  NULL, NULL,       	    0,            0,           1,         1,        -1        -1 },
+	{ "Telegram", NULL,       NULL,       	    0,            0,           1,         1,         1        -1 },
+	{ "Discord",  NULL,       NULL,       	    0,            0,           1,         1,        -1        -1 },
 	{ NULL,       NULL,       "Event Tester",   0,            0,           0,         1,        -1        -1 },
 	{ NULL,      "spterm",    NULL,       	    SPTAG(0),     1,           1,         0,        -1        -1 },
 	{ NULL,      "spcalc",    NULL,       	    SPTAG(1),     1,           1,         0,        -1        -1 },
