54c1b6f92044cf9327243e729acb9497f3cc28dc
plump_albert <plumpalbert@gmail.com>
2020-09-08 14:57:24 +0300

    Added config.def.h to apply patch

--- config.def.h
+++ config.def.h
@@ -1,33 +1,29 @@
 /* See LICENSE file for copyright and license details. */
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappih    = 16;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 12;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 16;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 16;       /* vert outer gap between windows and screen edge */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
-
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
-static const char *altbarclass      = "Polybar"; /* Alternate bar class name */
-static const char *alttrayname      = "polybar";    /* Polybar tray instance name */
-static const char *altbarcmd        = "$HOME/bar.sh"; /* Alternate bar launch command */
-
-
 static const char *fonts[]          = {
-  "Space Mono:pixelsize=10",
-  "Noto Color Emoji:pixelpixelsize=10:antialias=true:autohint=true",
-  "FontAwesome:pixelpixelsize=10:antialias=true:autohint=true",
-  "Font Awesome 5 Free:pixelpixelsize=10:antialias=true:autohint=true",
-  "Font Awesome 5 Brands:pixelpixelsize=10:antialias=true:autohint=true",
-  "monospace:pixelpixelsize=10:antialias=true:autohint=true",
+  "Space Mono:pixelsize=10:antialias=true:autohint=true",
+  "Apple Color Emoji:pixelsize=10:antialias=true:autohint=true",
+  "JoyPixels:pixelsize=10:antialias=true:autohint=true",
+  "FontAwesome:pixelsize=10:antialias=true:autohint=true",
+  "Font Awesome 5 Free:pixelsize=10:antialias=true:autohint=true",
+  "Font Awesome 5 Brands:pixelsize=10:antialias=true:autohint=true",
+  "monospace:pixelsize=10:antialias=true:autohint=true",
 };
-static char dmenufont[]             = "Space Mono:pixelsize=12";
-
+static char dmenufont[]             = "JetBrainsMono Nerd Font:pixelsize=10";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
@@ -45,7 +41,7 @@ typedef struct {
 	const void *cmd;
 } Sp;
 const char *spcmd1[] = {"st", "-n", "spterm", "-g", "120x34", NULL };
-const char *spcmd2[] = {"st", "-n", "spcalc", "-f", "monospace:pixelsize=10", "-g", "50x20", "-e", "bc", "-lq", NULL };
+const char *spcmd2[] = {"st", "-n", "spcalc", "-g", "50x20", "-e", "bc", "-lq", NULL };
 static Sp scratchpads[] = {
 	/* name          cmd  */
 	{"spterm",      spcmd1},
@@ -61,24 +57,30 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	*/
 	/* class    instance      title       	 tags mask    isfloating   isterminal  noswallow xkb_layout monitor */
-	{ "Gimp",     NULL,       NULL,       	    1 << 8,       0,           0,         0,         0        -1 },
-	{ "RescueTime",NULL,      NULL,       	    0,            1,           0,         0,         0        -1 },
-	{ "St",       NULL,       NULL,       	    0,            0,           1,         0,         0        -1 },
-	{ "firefox",  NULL,       NULL,       	    0,            0,           1,         0,        -1        -1 },
-	{ "Google-chrome",  NULL, NULL,       	    0,            0,           1,         0,        -1        -1 },
-	{ "Telegram", NULL,       NULL,       	    0,            0,           1,         0,         1        -1 },
-	{ "Discord",  NULL,       NULL,       	    0,            0,           1,         0,        -1        -1 },
-	{ NULL,       NULL,       "Event Tester",   0,            0,           0,         1,        -1        -1 },
-	{ NULL,      "spterm",    NULL,       	    SPTAG(0),     1,           1,         0,        -1        -1 },
-	{ NULL,      "spcalc",    NULL,       	    SPTAG(1),     1,           1,         0,        -1        -1 },
-	{ "lxqt-openssh-askpass", NULL,    NULL,    0,            1,           1,         0,         0        -1 },
+	{ "Gimp",     NULL,       NULL,       	    1 << 8,       0,           0,         0,         0,        -1 },
+	{ "RescueTime",NULL,      NULL,       	    0,            1,           0,         1,         0,        -1 },
+	{ "St",       NULL,       NULL,       	    0,            0,           1,         0,         0,        -1 },
+	{ "firefox",  NULL,       NULL,       	    0,            0,           1,         1,        -1,        -1 },
+	{ "Google-chrome",  NULL, NULL,       	    0,            0,           1,         1,        -1,        -1 },
+	{ "Telegram", NULL,       NULL,       	    0,            0,           1,         1,         1,        -1 },
+	{ "Discord",  NULL,       NULL,       	    0,            0,           1,         1,        -1,        -1 },
+	{ NULL,       NULL,       "Event Tester",   0,            0,           0,         1,        -1,        -1 },
+	{ NULL,      "spterm",    NULL,       	    SPTAG(0),     1,           1,         0,        -1,        -1 },
+	{ NULL,      "spcalc",    NULL,       	    SPTAG(1),     1,           1,         0,        -1,        -1 },
+	{ "lxqt-openssh-askpass", NULL,    NULL,    0,            1,           1,         0,         0,        -1 },
+	{ "TelegramDesktop", NULL,    " ",          0,            1,           0,         1,         0,        -1 },
 };
 
 /* layout(s) */
 static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
-
+/* xkb frontend */
+static const int showkb = 1; // 0 means no xkb layout text
+static const char *xkb_layouts [] = {
+  "🇺🇸",
+  "🇷🇺"
+};
 #define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
 #include "vanitygaps.c"
 static const Layout layouts[] = {
@@ -144,8 +146,7 @@ static Key keys[] = {
 	{ MODKEY,			XK_0,		view,		{.ui = ~0 } },
 	{ MODKEY|ShiftMask,		XK_0,		tag,		{.ui = ~0 } },
 
-	{ MODKEY,			XK_Tab,		view,		{0} },
-	/* { MODKEY|ShiftMask,		XK_Tab,		spawn,		SHCMD("") }, */
+  /* { MODKEY|ShiftMask,		XK_Tab,		spawn,		SHCMD("") }, */
 	{ MODKEY,			XK_q,		killclient,	{0} },
 	{ MODKEY|ShiftMask,		XK_q,		spawn,		SHCMD("rofi_run -l") },
 	{ MODKEY,			XK_w,		spawn,		SHCMD("$BROWSER") },
@@ -161,7 +162,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,		XK_i,		setlayout,	{.v = &layouts[7]} }, /* centeredfloatingmaster */
 	{ MODKEY,			XK_o,		incnmaster,     {.i = +1 } },
 	{ MODKEY|ShiftMask,		XK_o,		incnmaster,     {.i = -1 } },
-	{ MODKEY,			XK_p,			spawn,		SHCMD("mpc toggle") },
+	{ MODKEY,			XK_Tab,		view,		{0} },
 	{ Mod4Mask,		XK_Tab,		view,		{0} },
 	/* { MODKEY|ShiftMask,		XK_backslash,		spawn,		SHCMD("") }, */
 
@@ -275,7 +276,7 @@ static Button buttons[] = {
 	{ ClkStatusText,        0,              Button4,        sigdwmblocks,   {.i = 4} },
 	{ ClkStatusText,        0,              Button5,        sigdwmblocks,   {.i = 5} },
 	{ ClkStatusText,        ShiftMask,      Button1,        sigdwmblocks,   {.i = 6} },
-	{ ClkStatusText,        ShiftMask,      Button3,        spawn,          SHCMD("st -e nvim ~/.local/src/dwmblocks/config.h") },
+	{ ClkStatusText,        ShiftMask,      Button3,        spawn,          SHCMD("st -e nvim ~/sources/blocks/config.h") },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        defaultgaps,	{0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
@@ -288,22 +289,3 @@ static Button buttons[] = {
 	{ ClkTagBar,		0,		Button4,	shiftview,	{.i = -1} },
 	{ ClkTagBar,		0,		Button5,	shiftview,	{.i = 1} },
 };
-
-static const char *ipcsockpath = "/tmp/dwm.sock";
-static IPCCommand ipccommands[] = {
-  IPCCOMMAND(  view,                1,      {ARG_TYPE_UINT}   ),
-  IPCCOMMAND(  toggleview,          1,      {ARG_TYPE_UINT}   ),
-  IPCCOMMAND(  tag,                 1,      {ARG_TYPE_UINT}   ),
-  IPCCOMMAND(  toggletag,           1,      {ARG_TYPE_UINT}   ),
-  IPCCOMMAND(  tagmon,              1,      {ARG_TYPE_UINT}   ),
-  IPCCOMMAND(  focusmon,            1,      {ARG_TYPE_SINT}   ),
-  IPCCOMMAND(  focusstack,          1,      {ARG_TYPE_SINT}   ),
-  IPCCOMMAND(  zoom,                1,      {ARG_TYPE_NONE}   ),
-  IPCCOMMAND(  incnmaster,          1,      {ARG_TYPE_SINT}   ),
-  IPCCOMMAND(  killclient,          1,      {ARG_TYPE_SINT}   ),
-  IPCCOMMAND(  togglefloating,      1,      {ARG_TYPE_NONE}   ),
-  IPCCOMMAND(  setmfact,            1,      {ARG_TYPE_FLOAT}  ),
-  IPCCOMMAND(  setlayoutsafe,       1,      {ARG_TYPE_PTR}    ),
-  IPCCOMMAND(  quit,                1,      {ARG_TYPE_NONE}   )
-};
-
